0f494937eb61060fa0ac6a6440a3396ba01fc8b9
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-10-28 01:13:08 +0300

    done both tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const li = document.createElement("li");
+  li.classList.add(".gallery__item");
+  li.innerHTML = ` <a class="gallery__link" href="${image.original}">
+    <img class ="gallery__image" src="${image.preview}" alt"=${image.description} data-source="${image.original}"/>
+    </a>`;
+  galleryEl.append(li);
+});
+
+galleryEl.addEventListener("click", openImageInLigthox);
+
+function openImageInLigthox(e) {
+  const clickedOn = e.target;
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+
+  basicLightbox
+    .create(
+      `<img src="${clickedOn.dataset.source}" width="1400" heigth= "900"/>`
+    )
+    .show();
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const li = document.createElement("li");
+  li.classList.add(".gallery__item");
+  li.innerHTML = ` <a class="gallery__link" href="${image.original}">
+    <img class ="gallery__image" src="${image.preview}" alt"=${image.description}" title="${image.description}"/>
+    </a>`;
+  galleryEl.append(li);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+});
+
